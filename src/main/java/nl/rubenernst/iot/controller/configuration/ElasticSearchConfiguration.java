e857c7d3ba2301693ea7798ce57b13750562e3ca
Ruben Ernst <rubenernst@gmail.com>
2016-03-21 14:34:50 +0100

    Removed host variable

--- src/main/java/nl/rubenernst/iot/controller/configuration/ElasticSearchConfiguration.java
+++ src/main/java/nl/rubenernst/iot/controller/configuration/ElasticSearchConfiguration.java
@@ -16,13 +16,13 @@ public class ElasticSearchConfiguration {
     private TransportClient transportClient;
 
     @Bean
-    public TransportClient client(@Value("${elasticsearch.cluster.name}") String clusterName, @Value("${elasticsearch.cluster.host}") String host, @Value("${elasticsearch.cluster.port}") int port) throws UnknownHostException {
+    public TransportClient client(@Value("${elasticsearch.cluster.name}") String clusterName, @Value("${elasticsearch.cluster.host}") String host) throws UnknownHostException {
         transportClient = TransportClient.builder()
                 .settings(Settings.builder()
                         .put("cluster.name", clusterName)
                         .build())
                 .build()
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), 9300));
         return transportClient;
     }
 
